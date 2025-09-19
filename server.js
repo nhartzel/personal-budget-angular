9ab178b55f89951052f75d87cfb900c9ba32e945
nhartzel <nhartzel@uncc.edu>
2025-09-19 14:33:44 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -7,26 +7,17 @@ const port = 3000;
 
 app.use(cors());
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+const data = require("./data.json");
 
 
-app.get('/budget', (req, res) => {
-    res.json(budget);
+app.use('/', express.static('public'))
+
+//app.get('/budget', (req, res) => {
+//    res.json(budget);
+//});
+
+app.get("/budget", (req, res) => {
+    res.json(data);
 });
 
 app.listen(port, () => {
